7619676d7d4192607df90ce50d84e06611e72bc5
Jesse Bellingham <5078290+jbellingham@users.noreply.github.com>
2024-09-24 20:11:28 +1000

    Replace extensions with import from extensions.nix

--- modules/common/gui-apps/vscode/default.nix
+++ modules/common/gui-apps/vscode/default.nix
@@ -4,26 +4,6 @@
     enable = true;
     package = pkgs.vscode.fhs;
     extensions =
-      with pkgs.vscode-extensions;
-      [
-        eamodio.gitlens
-        jnoortheen.nix-ide
-        ms-vscode.makefile-tools
-        wakatime.vscode-wakatime
-        ms-vscode.makefile-tools
-        arrterian.nix-env-selector
-        # kisstkondoros.vscode-codemetrics
-        # dbaeumer.vscode-eslint
-        vincaslt.highlight-matching-tag
-        christian-kohler.npm-intellisense
-      ]
-      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "codeium";
-        publisher = "codeium";
-        version = "1.21.1";
-        sha256 = "00xvqigqn3fwwxi95vcvy0pf69x6kb8jdvs6r7gn7k2jig88ahhz";
-      }
-      ];
+      pkgs.vscode-utils.extensionsFromVscodeMarketplace (import ./extensions.nix).extensions;
   };
 }
